cec329a5b181191950a1ad22928d99e050fb06a7
Clement-Plancon <86836879+Clement-Plancon@users.noreply.github.com>
2021-10-27 09:35:40 +0200

    commit 26/10/2021 un peu trash, à debug

--- app.js
+++ app.js
@@ -1,5 +1,31 @@
+let $map = document.querySelector('#map')
+
+class LeafletMap {
+
+    load(element) {
+        L.map(element)
+    }
+
+}
+
+const initMap = function() {
+    let map = new LeafletMap()
+    map.load($map)
+}
+
+if ($map !== null) {
+    initMap()
+}
+
 let mymap = L.map('map').setView([51.505, -0.09], 13);
-L.tileLayer('//{s}.tile.openstreetmap.fr/osmfr/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
+    accessToken: 'pk.eyJ1IjoiY2xlbTEyMyIsImEiOiJja3Y4NjFzejgwbGMxMnJxdXBkOGsycGNlIn0.Geq-rDGWXPV5Qp3Awg0Gaw',
+    id: 'mapbox.streets'
 }).addTo(mymap);
+
+L.popup()
+    .setLatLng(51.505, -0.09)
+    .setContent('<p>Hello world!<br />This is a nice popup.</p>')
+    .openOn(map);
